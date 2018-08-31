26da22a228efdc40c00d698434c73b8dad3d54ec
Brian Shanley <brian.shanley@wipro.com>
2018-08-31 13:04:29 +0100

    recurse through pages & calculate results

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 Initially will attempt to scrape carzone.ie for specific car brand and model and return result range in an object consisting of Title and Price.
 
 With Node installed, install puppeteer
+
 `npm install --save puppeteer`
 
-to run: `node carzone.js [car make] [car model]`
-e.g. `node carzone.js ford focus`
+to run: 
+
+`node carzone.js [car make] [car model]`
+e.g. 
+
+`node carzone.js ford focus`
 
 ## TODO 
   * Handle pagination
--- carzone.js
+++ carzone.js
@@ -1,9 +1,8 @@
 const puppeteer = require('puppeteer');
 
-// print process.argv
 carName = process.argv[2];
 modelName = process.argv[3];
-console.log(`Lookup ${carName} ${modelName}`);
+// console.log(`Lookup ${carName} ${modelName}`);
 const url = `https://www.carzone.ie/used-cars/${carName}/${modelName}`;
 
 async function getPic(url) {
@@ -22,46 +21,103 @@ async function getPic(url) {
 
   // Wait til there's no more pagination
   // await page.waitForFunction(() => !document.querySelector('#search-results > div.search-state.clearfix.top > div.paginate-results > ul > li.next-page.paginate.last-item > a > span'));
-  console.log('no more pagination')
+  // console.log('no more pagination')
 
   await page.waitFor(2000);
+
   const resultDetails = await page.evaluate(() => document.querySelector('#search-results > div.search-state.clearfix.top > div.advert-counter.search-info').innerText);
   let numberOfResults = resultDetails.split(/(\s+)/);
   numberOfResults = numberOfResults[4];
+  console.log(`${numberOfResults} results`);
+  let numberScraped = 0;
+  let allData = [];
 
-  const result = await page.evaluate(() => {
-    let data = []; // Create an empty array
-    let title = "test";
-    let price = 0;
-    let count = 0;
-    // Loop through each proudct
-    let elements = document.querySelectorAll('.advert-inner');
-    for (var element of elements) { // Loop through each proudct
-      count++;
-      title = element.childNodes[5].children[0].innerText.replace(/^\s+|\s+$/g, ''); // Select the title
-      year = element.childNodes[5].children[1].innerText.replace(/^\s+|\s+$/g, ''); // Select the title
-      extra = element.childNodes[5].children[2].innerText.replace(/^\s+|\s+$/g, ''); // Select the title
-      price = element.childNodes[7].children[0].innerText.replace(/^\s+|\s+$/g, ''); // Select the title
-      priceNumber = price.replace(/[^0-9.-]+/g, "");
-
-      data.push({ count, title, price: priceNumber, year, extra }); // Push an object with the data onto our array
+  if (numberOfResults > 30) {
+    while (numberScraped <= (numberOfResults / 30)) {
+      numberScraped++;
+      console.log(numberScraped)
+      let newPage = await getPageData();
+      allData = [...allData, ...newPage];
     }
-    return data; // Return our data array
+  } else {
+    let newPage = await getPageData();
+    allData = newPage;
+  }
+  function getPageData() {
+
+    return page.evaluate(() => {
+      let data = []; // Create an empty array
+      let title = "test";
+      let price = 0;
+      let count = 0;
+      // Loop through each proudct
+      let elements = document.querySelectorAll('.advert-inner');
+      for (var element of elements) { // Loop through each proudct
+        count++;
+        title = element.childNodes[5].children[0].innerText.replace(/^\s+|\s+$/g, '');
+        year = element.childNodes[5].children[1].innerText.replace(/^\s+|\s+$/g, '');
+        extra = element.childNodes[5].children[2].innerText.replace(/^\s+|\s+$/g, '');
+        price = element.childNodes[7].children[0].innerText.replace(/^\s+|\s+$/g, '');
+        priceNumber = price.replace(/[^0-9.-]+/g, "");
+
+        data.push({ count, title, price: priceNumber, year, extra }); // Push an object with the data onto our array
+      }
+      return data; // Return our data array
+    });
+
+  }
+
+
+  // let priceArray = allData.map(car => car.price).sort();
+  let priceArray = allData.map((car) => {
+    console.log('here', car.price);
+    return car.price
+  });
+  priceArray = priceArray.sort();
+  priceArray = priceArray.filter((x) => {
+    return x > 0 && x < 99999;
   });
 
-  console.log(`Found ${numberOfResults} cars`);
-  let priceArray = result.map(car => car.price).sort();
+  console.log('Prices: ' + priceArray);
+  console.log(`${priceArray.length} cars`);
 
-  console.log('Prices', priceArray);
+  let medianPrice = getMedian(priceArray);
+  let averagePrice = getAverage(priceArray);
+
+  function getMedian(priceArray) {
+    var half = Math.floor(priceArray.length / 2);
+
+    if (parseInt(priceArray.length) % 2) {
+      console.log('media value is %2', priceArray[half]);
+      return priceArray[half];
+    }
+    else {
+      console.log('media value is NOT %2', parseInt(priceArray[half]), parseInt(priceArray[half - 1]), (priceArray[half - 1] + priceArray[half]));
+      return ((parseInt(priceArray[half - 1]) + parseInt(priceArray[half])) / 2.0);
+    }
+  }
+
+  function getAverage(priceArray) {
+    var sum = 0;
+    for (var i = 0; i < priceArray.length; i++) {
+      sum += parseInt(priceArray[i], 10); //don't forget to add the base
+    }
+
+    return sum / priceArray.length;
+  }
+
+  console.log(`price range: ${priceArray[0]} - ${priceArray[priceArray.length - 1]}`);
+  console.log(`median price: ${medianPrice}`);
+  console.log(`average price: ${averagePrice}`);
 
   await page.screenshot({ path: `images/carzone-${carName}-${modelName}.png` });
 
-  let scrape = async () => {
-    return 'test';
-  };
-  scrape().then((value) => {
-    console.log(value); // Success!
-  });
+  // let scrape = async () => {
+  //   return 'test';
+  // };
+  // scrape().then((value) => {
+  //   console.log(value); // Success!
+  // });
 
   await browser.close();
 }
